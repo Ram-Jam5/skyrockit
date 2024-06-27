64b2a13d592c3f078d27961894bf3e401c71663a
Ram-Jam5 <edgeillusive@gmail.com>
2024-06-27 09:50:32 -0500

    add show page for applications

--- controllers/applications.js
+++ controllers/applications.js
@@ -28,10 +28,22 @@ router.post('/', async (req, res) => {
     
         await currentUser.save();
         res.redirect(`/users/${currentUser._id}/applications`);
-
     } catch(error) {
         console.log(error);
         res.redirect('/');
+    }
+});
+
+router.get('/:applicationId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        const application = currentUser.applications.id(req.params.applicationId);
+        res.render('applications/show.ejs', {
+            application: application,
+        });
+    } catch (error) {
+        console.log(error);
+        res.redirect('/')
     }
 });
 
